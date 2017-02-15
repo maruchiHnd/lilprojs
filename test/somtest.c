1794e7865c5a473053b8444afcb659321fe2ff21
DARKYGNU <darkygnu@darkygnu--SYSV>
2017-02-16 00:59:48 +0300

    update cd

--- test/somtest.c
+++ test/somtest.c
@@ -4,11 +4,21 @@
  ******************************************************/
 
 
-#include <stdlib.io>
+#include <stdio.h>
 
+void askinput(void);
 
 int main(int argc, char *argv[])
 {
     //needs some movings thins here
     return 0;
+}
+
+/**
+ *
+ *
+ */
+void askinput()
+{
+   //get some from user 
 }
